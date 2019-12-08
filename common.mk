e0c70304ffdc6e0027b4efc30e2bcadc7729f772
Ethan Chen <intervigil@gmail.com>
2019-12-08 15:24:31 -0800

    sm7325-common: Remove leftover PRODUCT_VENDOR_MOVE_ENABLED directive
    Change-Id: I07e660f6fab0734c8e590e86862406c043cfe9dd

--- common.mk
+++ common.mk
@@ -402,7 +402,5 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/wifi/p2p_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/p2p_supplicant_overlay.conf \
     $(LOCAL_PATH)/wifi/wpa_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant_overlay.conf
 
-PRODUCT_VENDOR_MOVE_ENABLED := true
-
 # Inherit from vendor blobs
 $(call inherit-product, vendor/motorola/sm7325-common/sm7325-common-vendor.mk)
