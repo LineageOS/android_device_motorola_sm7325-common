a3dd6d4d079ef8fb2c1c922f958fafcc618f4d7e
Vinay Verma <vvinay@codeaurora.org>
2021-02-01 10:47:51 +0530

    sm8250-common: Enable PAC and BTI
    Change-Id: I014eadcd8b59260ad59e9a1085ccf974404590d0

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -20,7 +20,7 @@ COMMON_PATH := device/motorola/sm8250-common
 
 # Architecture
 TARGET_ARCH := arm64
-TARGET_ARCH_VARIANT := armv8-a
+TARGET_ARCH_VARIANT := armv8-a-branchprot
 TARGET_CPU_ABI := arm64-v8a
 TARGET_CPU_ABI2 :=
 TARGET_CPU_VARIANT := generic
