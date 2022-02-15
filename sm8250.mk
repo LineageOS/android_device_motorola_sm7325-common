4060a1c051959de70cbedfa84a5a60c1938b07e9
SGCMarkus <markusornik@gmail.com>
2022-02-15 22:57:45 +0100

    sm8250-common: build qti vibrator service

--- sm8250.mk
+++ sm8250.mk
@@ -286,4 +286,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     vndservicemanager
 
+# Vibrator
+PRODUCT_PACKAGES += \
+    vendor.qti.hardware.vibrator.service
 
+PRODUCT_COPY_FILES += \
+    vendor/qcom/opensource/vibrator/excluded-input-devices.xml:$(TARGET_COPY_OUT_VENDOR)/etc/excluded-input-devices.xml
