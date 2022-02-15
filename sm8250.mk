37620b3e135b17d10dfcb4a6023eb13725e21dae
SGCMarkus <markusornik@gmail.com>
2022-02-15 22:36:09 +0100

    sm8250-common: build qti power service

--- sm8250.mk
+++ sm8250.mk
@@ -229,3 +229,7 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.vulkan.deqp.level-2020-03-01.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.vulkan.deqp.level.xml \
     frameworks/native/data/etc/handheld_core_hardware.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/handheld_core_hardware.xml \
     frameworks/native/data/etc/unavail.android.hardware.sensor.barometer.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/unavail.android.hardware.sensor.barometer.xml
+
+# Power
+PRODUCT_PACKAGES += \
+    android.hardware.power-service-qti
