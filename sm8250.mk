e6e3a55871618ecb2be4974a0dcf12ac33ab6a25
SGCMarkus <markusornik@gmail.com>
2022-02-15 22:28:31 +0100

    sm8250-common: build atrace service

--- sm8250.mk
+++ sm8250.mk
@@ -45,6 +45,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES_DEBUG += \
     bootctl
 
+# Atrace
+PRODUCT_PACKAGES += \
+    android.hardware.atrace@1.0-service
+
 # Audio
 PRODUCT_PACKAGES += \
     android.hardware.audio@6.0-impl \
