1056fc09edc8661501d8e8f51f0e1c7e79772516
SGCMarkus <markusornik@gmail.com>
2022-02-17 11:32:41 +0100

    sm8250-common: config.fs: add moto GIDs

--- config.fs
+++ config.fs
@@ -22,6 +22,30 @@ value:2907
 [AID_VENDOR_FASTRPC]
 value:2908
 
+[AID_VENDOR_BRIDGE_RW]
+value:5001
+
+[AID_VENDOR_PWRIC]
+value:5101
+
+[AID_VENDOR_TCMD]
+value:5104
+
+[AID_VENDOR_DBVC]
+value:5114
+
+[AID_VENDOR_FINGERP]
+value:5115
+
+[AID_VENDOR_DTV]
+value:5119
+
+[AID_VENDOR_MOD]
+value:5120
+
+[AID_VENDOR_PROD_PERSIST]
+value:5121
+
 [bt_firmware/]
 mode: 0771
 user: AID_SYSTEM
