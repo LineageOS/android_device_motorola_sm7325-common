0fe53f415a4eb49552bacae7c178357591d0e765
dianlujitao <dianlujitao@lineageos.org>
2022-02-17 18:28:04 +0800

    sm8250-common: sepolicy: Fix build for S
    Change-Id: Ic0c822ce146bee4d09a62306d4573f557acd186e

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -207,6 +207,7 @@ ENABLE_VENDOR_RIL_SERVICE := true
 
 # SELinux
 include device/qcom/sepolicy_vndr/SEPolicy.mk
+BUILD_BROKEN_VENDOR_PROPERTY_NAMESPACE := true
 BOARD_VENDOR_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/vendor
 PRODUCT_PRIVATE_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/private
 PRODUCT_PUBLIC_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/public
