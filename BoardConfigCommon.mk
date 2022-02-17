94757d6254dc0a556c8ab601516fa148a775fce0
SGCMarkus <markusornik@gmail.com>
2022-02-17 20:26:00 +0100

    sm8250-common: add a device framework compatibility matrix file

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -154,6 +154,7 @@ TARGET_FS_CONFIG_GEN := $(COMMON_PATH)/config.fs
 
 # HIDL
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
+    $(COMMON_PATH)/device_framework_matrix.xml \
     vendor/lineage/config/device_framework_matrix.xml
 DEVICE_MATRIX_FILE := $(COMMON_PATH)/compatibility_matrix.xml
 DEVICE_MANIFEST_FILE := $(COMMON_PATH)/manifest.xml
