06edd5442785c4bb0b3c204ed480243d8da7f9a0
SGCMarkus <markusornik@gmail.com>
2022-02-17 15:38:15 +0100

    sm8250-common: remove duplicated prop

--- sm8250.mk
+++ sm8250.mk
@@ -262,7 +262,6 @@ PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
     ro.surface_flinger.has_HDR_display=true \
     ro.surface_flinger.has_wide_color_display=true \
     ro.surface_flinger.protected_contents=true \
-    ro.surface_flinger.set_touch_timer_ms=200 \
     ro.surface_flinger.use_color_management=true \
     ro.surface_flinger.wcg_composition_dataspace=143261696
 
