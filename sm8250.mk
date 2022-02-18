818a89d1250b4b9506bfa11c3e264d92967e9fed
SGCMarkus <markusornik@gmail.com>
2022-02-18 12:59:02 +0100

    sm8250-common: enable updatable apex

--- sm8250.mk
+++ sm8250.mk
@@ -17,6 +17,9 @@
 PRODUCT_BUILD_SUPER_PARTITION := false
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
+# Enable updating of APEXes
+$(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
+
 # Installs gsi keys into ramdisk, to boot a GSI with verified boot.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
 
