907c9133c9f8de81c35b56bb7383c5084bdde6fa
SGCMarkus <markusornik@gmail.com>
2022-02-18 17:22:16 +0100

    sm8250-common: add sec_config

--- sm8250.mk
+++ sm8250.mk
@@ -198,6 +198,10 @@ PRODUCT_PACKAGES += \
     libipanat \
     liboffloadhal
 
+# IPC router config
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/sec_config:$(TARGET_COPY_OUT_VENDOR)/etc/sec_config
+
 # Keymaster
 PRODUCT_PACKAGES += \
     android.hardware.keymaster@4.1.vendor
