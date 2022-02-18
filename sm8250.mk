92f77e934692060971587ba2de49a2c12ff7183c
SGCMarkus <markusornik@gmail.com>
2022-02-18 17:24:41 +0100

    sm8250-common: add public.libraries from stock

--- sm8250.mk
+++ sm8250.mk
@@ -40,6 +40,11 @@ PRODUCT_PACKAGES += \
     checkpoint_gc \
     otapreopt_script
 
+# Additional native libraries
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/public.libraries.txt:$(TARGET_COPY_OUT_VENDOR)/etc/public.libraries.txt \
+    $(LOCAL_PATH)/configs/public.libraries-qti.txt:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/public.libraries-qti.txt
+
 # Boot control
 PRODUCT_PACKAGES += \
     android.hardware.boot@1.1-impl-qti \
