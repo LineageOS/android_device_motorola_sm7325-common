4601793fdfa2e7d042b98abce4e2cbb3fbced621
SGCMarkus <markusornik@gmail.com>
2022-02-19 15:42:35 +0100

    sm8250-common: build libgui_vendor from source

--- sm8250.mk
+++ sm8250.mk
@@ -128,6 +128,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service_64 \
+	libgui_vendor \
     Snap \
     vendor.qti.hardware.camera.postproc@1.0.vendor
 
