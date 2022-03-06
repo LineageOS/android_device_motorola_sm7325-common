3b673817793cc05ebb42aee09d652b98810f723d
SGCMarkus <markusornik@gmail.com>
2022-03-06 20:44:40 +0100

    sm8250-common: build sdm common livedisplay for picture adjustment

--- sm8250.mk
+++ sm8250.mk
@@ -248,6 +248,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.lights-service.motokona
 
+# LiveDisplay
+PRODUCT_PACKAGES += \
+    vendor.lineage.livedisplay@2.0-service-sdm
+
 # Media
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/media/media_codecs.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs.xml \
