81c175bd93dc65ba75ca50b9bfd331c0c439297b
SGCMarkus <markusornik@gmail.com>
2022-03-26 23:27:49 +0100

    sm7325-common: gps: update to LA.UM.9.12.r1-13500.01-SMxx50.QSSI12.0

--- gps/batching/BatchingAdapter.cpp
+++ gps/batching/BatchingAdapter.cpp
@@ -37,9 +37,7 @@
 using namespace loc_core;
 
 BatchingAdapter::BatchingAdapter() :
-    LocAdapterBase(0,
-                   LocContext::getLocContext(LocContext::mLocationHalName),
-                   false, nullptr, true),
+    LocAdapterBase(0, LocContext::getLocContext(LocContext::mLocationHalName)),
     mOngoingTripDistance(0),
     mOngoingTripTBFInterval(0),
     mTripWithOngoingTBFDropped(false),
@@ -52,10 +50,6 @@ BatchingAdapter::BatchingAdapter() :
     LOC_LOGD("%s]: Constructor", __func__);
     readConfigCommand();
     setConfigCommand();
-
-    // at last step, let us inform adapater base that we are done
-    // with initialization, e.g.: ready to process handleEngineUpEvent
-    doneInit();
 }
 
 void
