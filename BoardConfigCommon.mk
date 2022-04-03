599b343fcf72d3a5c0f61a874ae78f0efce598a6
SGCMarkus <markusornik@gmail.com>
2022-04-03 21:51:42 +0200

    sm7325-common: kernel: use proper defconfigs
    Change-Id: I26ed6f5f5c75df079a7daf7532824472ca12500f

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -61,7 +61,7 @@ BOARD_MKBOOTIMG_ARGS += --header_version $(BOARD_BOOT_HEADER_VERSION)
 TARGET_KERNEL_ADDITIONAL_FLAGS := DTC_EXT=$(shell pwd)/prebuilts/misc/linux-x86/dtc/dtc LLVM=1
 TARGET_KERNEL_SOURCE := kernel/motorola/sm7325
 TARGET_KERNEL_CLANG_COMPILE := true
-TARGET_KERNEL_CONFIG := gki_defconfig
+TARGET_KERNEL_CONFIG := vendor/lahaina-qgki_defconfig vendor/lineage_moto-lahaina.config
 
 # Platform
 BOARD_USES_QCOM_HARDWARE := true
