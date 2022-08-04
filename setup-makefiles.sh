c75f08dd0f5bb0721162841636a516483186f3ec
Marc Bourgoin <themard85@gmail.com>
2022-08-03 23:57:01 -0600

    sm7325-common: Fix setup makefiles for berlin
    Change-Id: I7c983f470594025cd6e90c217641fb4e70fa4d93

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -25,7 +25,7 @@ source "${HELPER}"
 setup_vendor "${DEVICE_COMMON}" "${VENDOR}" "${ANDROID_ROOT}" true
 
 # Warning headers and guards
-write_headers "pstar nio"
+write_headers "berlin"
 
 # The standard common blobs
 write_makefiles "${MY_DIR}/proprietary-files.txt" true
