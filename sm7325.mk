887dd57b09c1e582cd7eda63f50cd3db4e8cd336
Nolen Johnson <johnsonnolen@gmail.com>
2022-08-09 10:54:48 -0400

    sm7325-common: Swap to QTI USB init scripts
    Change-Id: Ibb01a6a8b9817b01a8db397cdf82a89659562d62

--- sm7325.mk
+++ sm7325.mk
@@ -474,7 +474,11 @@ PRODUCT_PACKAGES_DEBUG += \
 
 # USB
 PRODUCT_PACKAGES += \
-    android.hardware.usb@1.3-service-qti
+    android.hardware.usb@1.3-service-qti \
+    init.qcom.usb.rc \
+    init.qcom.usb.sh
+
+PRODUCT_SOONG_NAMESPACES += vendor/qcom/opensource/usb/etc
 
 # Vendor service manager
 PRODUCT_PACKAGES += \
