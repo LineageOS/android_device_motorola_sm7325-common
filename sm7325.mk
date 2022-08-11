c159f4a88782c6db2a5ef0343c95d94fd228b497
SGCMarkus <markusornik@gmail.com>
2022-08-11 14:01:31 +0200

    sm7325-common: use media_codecs_yupik_v0.xml also for v1
    both ro.media.xml_variant.codecs and ro.media.xml_variant.codecs_performance are
    set to _yupik_v1, but media_codecs_yupik_v1.xml, but it does not contain any
    video codecs.
    This causes video playback (e.g. Youtube) to only support up to 1080p60 (stuttering)
    without HDR. Trying to record videos in >1080p will also result in the camera HAL
    crashing.
    
    Using the v0 config for v1 too fixes both issues. Youtube now supports up
    to 4K60 with HDR, and camera recording in >1080p works.
    
    Change-Id: I498c2fca380618dea4768e9d3fcda77585b1de97

--- sm7325.mk
+++ sm7325.mk
@@ -262,7 +262,7 @@ PRODUCT_PACKAGES += \
 # Media
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/media/media_codecs_yupik_v0.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_yupik_v0.xml \
-    $(LOCAL_PATH)/media/media_codecs_yupik_v1.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_yupik_v1.xml \
+    $(LOCAL_PATH)/media/media_codecs_yupik_v0.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_yupik_v1.xml \
     $(LOCAL_PATH)/media/media_codecs_performance_yupik_v0.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_performance_yupik_v0.xml \
     $(LOCAL_PATH)/media/media_codecs_performance_yupik_v1.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_performance_yupik_v1.xml \
     $(LOCAL_PATH)/media/media_profiles_V1_0.xml:$(TARGET_COPY_OUT_ODM)/etc/media_profiles_V1_0.xml
