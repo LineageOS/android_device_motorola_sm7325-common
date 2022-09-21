5853965fb2b1fbbc56a305c0971d03343d25d317
AdarshGrewal <adarshgrewal@gmail.com>
2022-09-21 12:08:51 +0530

    sm7325-common: Update blobs from Dubai 12-S1RD32M.55-67-4
    Plus repatched ims.apk here
    https://github.com/SGCMarkus/moto-ims/tree/S1RDS32.55-67-4
    
    Also unpinned ImsRcsService and WFD
    
    Co-authored-by: SGCMarkus <markusornik@gmail.com>
    Co-authored-by: Marc Bourgoin <themard85@gmail.com>
    Change-Id: I61d64a3d1493a5be8e1f48587ccc52814ab0588d

--- extract-files.sh
+++ extract-files.sh
@@ -66,6 +66,9 @@ function blob_fixup() {
         system_ext/etc/permissions/moto-telephony.xml)
             sed -i "s#/system/#/system_ext/#" "${2}"
             ;;
+        vendor/etc/vintf/manifest/vendor.dolby.media.c2@1.0-service.xml)
+            sed -ni '/default1/!p' "${2}"
+            ;;
         vendor/lib64/vendor.qti.hardware.camera.postproc@1.0-service-impl.so)
             hexdump -ve '1/1 "%.2X"' "${2}" | sed "s/130A0094/1F2003D5/g" | xxd -r -p > "${TMPDIR}/${1##*/}"
             mv "${TMPDIR}/${1##*/}" "${2}"
