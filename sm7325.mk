43e9be81955011f156a44bdc672e127f24995102
SGCMarkus <markusornik@gmail.com>
2022-09-27 17:57:22 +0200

    sm7325-common: switch to developer_gsi_keys
    Change-Id: Iad357bd7dc0b6ae15e381c23a7ddf7b86f50f14e

--- sm7325.mk
+++ sm7325.mk
@@ -24,7 +24,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 
 # Installs gsi keys into ramdisk, to boot a GSI with verified boot.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/developer_gsi_keys.mk)
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
