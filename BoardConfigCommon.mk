c48cb1d8257da38e40aaaf12311d1c8a928a6bee
electimon <electimon@gmail.com>
2023-03-12 03:04:37 +0000

    sm7325-common: Rename manifest_yupik.xml to manifest.xml
    * CAF only does this because they use one device tree
      for all 3 SoCs, vendor/qcom/lahaina. We use one -common
      tree with device trees inheriting it. There is no need
      to have manifest_yupik.xml.
    
    Change-Id: I133f19725d523ce2e4acb17114b687f9e5757019
    Signed-off-by: electimon <electimon@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -127,9 +127,7 @@ DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
     $(COMMON_PATH)/device_framework_matrix.xml \
     vendor/lineage/config/device_framework_matrix.xml
 DEVICE_MATRIX_FILE := $(COMMON_PATH)/compatibility_matrix.xml
-DEVICE_MANIFEST_SKUS := yupik
-DEVICE_MANIFEST_YUPIK_FILES += $(COMMON_PATH)/manifest_yupik.xml
-#DEVICE_MANIFEST_FILE += $(COMMON_PATH)/manifest.xml
+DEVICE_MANIFEST_FILE += $(COMMON_PATH)/manifest.xml
 
 # Metadata
 BOARD_USES_METADATA_PARTITION := true
