029151f194e0808e5ba30fd0c793c4d02b241411
LuK1337 <priv.luk@gmail.com>
2023-10-21 16:34:40 +0200

    sm7325-common: Build android.frameworks.sensorservice@1.0.vendor
    Required by libdpps.
    
    Change-Id: Iaf821dcbb07d7820f2fc3ff5ca9cca5e973360f2

--- common.mk
+++ common.mk
@@ -428,6 +428,7 @@ PRODUCT_PACKAGES += \
 
 # Sensors
 PRODUCT_PACKAGES += \
+    android.frameworks.sensorservice@1.0.vendor \
     android.hardware.sensors@2.0-service.multihal \
     libsensorndkbridge
 
