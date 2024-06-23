59f8b10b34847615c8963c5ef68b83e8015df2e9
Bruno Martins <bgcngm@gmail.com>
2024-06-23 21:58:49 +0100

    sm7325-common: Convert WiFi firmware symlink to install_symlink target
    The last bit that was preventing building hermetic images.
    
    Change-Id: I75bb7de4593a7992f6015317cf97e088beeba0cb

--- common.mk
+++ common.mk
@@ -534,6 +534,12 @@ PRODUCT_PACKAGES += \
     wpa_supplicant \
     wpa_supplicant.conf
 
+# WiFi firmware symlinks
+PRODUCT_PACKAGES += \
+    firmware_wlan_mac.bin_symlink \
+    firmware_WCNSS_qcom_cfg.ini_qca6490_symlink \
+    firmware_WCNSS_qcom_cfg.ini_qca6750_symlink
+
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/wifi/WCNSS_qcom_cfg.ini:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/WCNSS_qcom_cfg.ini \
     $(LOCAL_PATH)/wifi/p2p_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/p2p_supplicant_overlay.conf \
