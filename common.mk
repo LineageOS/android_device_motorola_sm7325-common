e4162b4989ba8a7646a7eddc3c2f68dca3dd5c2f
Georg Veichtlbauer <georg@vware.at>
2024-06-25 23:02:34 +0200

    sm7325-common: Build vendor variant of libpower
    Change-Id: Id7fe8a2598ffb93be4635424c86100a88aea8780

--- common.mk
+++ common.mk
@@ -445,6 +445,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.frameworks.sensorservice@1.0.vendor \
     android.hardware.sensors@2.0-service.multihal \
+    libpower.vendor \
     libsensorndkbridge
 
 # Soong namespaces
