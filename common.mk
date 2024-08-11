e62d0972842bbda00061343b0715b8d20b3e0106
Cosmin Tanislav <demonsingur@gmail.com>
2024-08-11 13:42:06 +0300

    sm7325-common: remove CellBroadcastReceiver overlay
    Values overlayed here do not exist.
    
    Change-Id: Ic6fb9fd733d8bff1bdd64b9d247ab4516c2d3e24

--- common.mk
+++ common.mk
@@ -32,7 +32,6 @@ PRODUCT_ENFORCE_RRO_TARGETS := *
 PRODUCT_PACKAGES += \
     FrameworksResCommon_Sys \
     CarrierConfigResCommon_Sys \
-    CellBroadcastReceiverResCommon_Sys \
     SystemUIResCommon_Sys \
     TelecommResCommon_Sys \
     TelephonyResCommon_Sys \
