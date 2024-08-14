d25f4025e3a579f2d42565a16d4d4142314ba7ec
Cosmin Tanislav <demonsingur@gmail.com>
2024-08-14 16:04:01 +0300

    sm7325-common: remove Telecomm overlays
    These are all the default values. It's also guarded
    by TARGET_FWK_SUPPORTS_FULL_VALUEADDS which we just disabled.
    
    Change-Id: I4bf30d4a12145a30feee6e560d415c1258ce5385

--- common.mk
+++ common.mk
@@ -31,7 +31,6 @@ PRODUCT_ENFORCE_RRO_TARGETS := *
 PRODUCT_PACKAGES += \
     FrameworksResCommon_Sys \
     SystemUIResCommon_Sys \
-    TelecommResCommon_Sys \
     TelephonyResCommon_Sys \
     WifiResCommon_Sys \
     FrameworksResTarget \
