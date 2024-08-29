4593d2dc8206766eae224ced687cc46939487cf4
Arian <arian.kulmer@web.de>
2024-08-29 18:29:56 +0200

    sm7325-common: Drop unused perf blobs
    Change-Id: I09e2c332102a5b67b7e1d74e9db3d64137e3010a

--- extract-files.py
+++ extract-files.py
@@ -41,7 +41,6 @@ lib_fixups: lib_fixups_user_type = {
         'vendor.qti.hardware.fm@1.0',
         'vendor.qti.hardware.wifidisplaysession@1.0',
         'vendor.qti.imsrtpservice@3.0',
-        'vendor.qti.qspmhal@1.0',
     ): lib_fixup_vendor_suffix,
     (
         'libofflinelog',
