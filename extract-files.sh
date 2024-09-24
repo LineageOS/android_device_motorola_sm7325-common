35a0f86366656f67a4567a11f2ed5ea9f2b2c81f
Marc Bourgoin <themard85@gmail.com>
2024-09-23 22:44:12 -0600

    sm7325-common: Update blob fixup for dolby c2 with a wildcard
    The fqname can change, so let's add a wildcard after the word default with fqname
    just to make sure it doesn't accidentally delete the instance line
    
    eg, on dubai A13:
            <fqname>@1.0::IComponentStore/default1</fqname>
    
    on dubai A14:
            <fqname>@1.0::IComponentStore/default9</fqname>
    
    Change-Id: I0c55a9781b398601ecbd6be7c327036fe4b1ac42

--- extract-files.sh
+++ extract-files.sh
@@ -75,7 +75,7 @@ function blob_fixup() {
             ;;
         vendor/etc/vintf/manifest/vendor.dolby.media.c2@1.0-service.xml)
             [ "$2" = "" ] && return 0
-            sed -ni '/default1/!p' "${2}"
+            sed -ni '/default.*fqname/!p' "${2}"
             ;;
         *)
             return 1
