74673ad46f9b0b8d3a8362e20388f94eaf8a4667
LuK1337 <priv.luk@gmail.com>
2024-10-12 17:42:02 +0200

    sm7325-common: Shim WFD with libgui_shim/libinput_shim
    Change-Id: I42bf06bc6b19942303031ef0147dafa6df127fe5

--- common.mk
+++ common.mk
@@ -528,9 +528,11 @@ PRODUCT_VENDOR_MOVE_ENABLED := true
 # WiFi Display
 PRODUCT_PACKAGES += \
     libavservices_minijail \
+    libgui_shim \
+    libinput_shim
     libnl \
     libpng.vendor \
-    libwfdaac_vendor
+    libwfdaac_vendor \
 
 PRODUCT_BOOT_JARS += \
     WfdCommon
