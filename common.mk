62dd237ef2ae51bcea56a32c666c1a6f7923bb03
Michael Bestas <mkbestas@gmail.com>
2024-12-29 04:23:16 +0200

    sm7325-common: Get rid of TARGET_RECOVERY_DEVICE_DIRS
    Change-Id: Id32dc00987178a6e9b0fd7fac0f4598f1b3be7e3

--- common.mk
+++ common.mk
@@ -190,6 +190,9 @@ $(foreach f,$(wildcard $(LOCAL_PATH)/rootdir/etc/init/*.rc),\
 $(foreach f,$(wildcard $(LOCAL_PATH)/rootdir/bin/*.sh),\
         $(eval PRODUCT_COPY_FILES += $(f):$(TARGET_COPY_OUT_VENDOR)/bin/$(notdir $f)))
 
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/recovery/root/init.recovery.qcom.rc:recovery/root/init.recovery.qcom.rc
+
 # IPACM
 PRODUCT_PACKAGES += \
     ipacm \
