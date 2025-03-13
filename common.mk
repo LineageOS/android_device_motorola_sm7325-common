882216cb0fc40fbe073da569220a209afea89ffd
Michael Bestas <mkbestas@lineageos.org>
2025-03-13 13:05:52 +0200

    sm7325-common: Move telephony privapp perms to vendor
    Change-Id: I7085a65cd889a266552dfcb1e4306a57a01fd36b

--- common.mk
+++ common.mk
@@ -335,9 +335,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_BOOT_JARS += \
     telephony-ext
 
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/telephony_system-ext_privapp-permissions-qti.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/telephony_system-ext_privapp-permissions-qti.xml
-
 # Thermal
 PRODUCT_PACKAGES += \
     android.hardware.thermal-service.qti
