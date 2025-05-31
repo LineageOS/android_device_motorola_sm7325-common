695cf315dd8d50b61f204f79b0c26bd0c612e301
Bruno Martins <bgcngm@gmail.com>
2025-05-31 13:01:12 +0100

    sm7325-common: Move and recategorize compat matrix and manifest flags
    Qualcomm also sets these in (common) product makefiles.
    
    Change-Id: I7139cba92454106fbca5f6105b966a43e70fe9d6

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -88,13 +88,6 @@ TARGET_FS_CONFIG_GEN := $(COMMON_PATH)/config.fs
 # GPS
 BOARD_VENDOR_QCOM_GPS_LOC_API_HARDWARE := default
 
-# HIDL
-DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += \
-    $(COMMON_PATH)/device_framework_matrix.xml \
-    hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml
-DEVICE_MATRIX_FILE := hardware/qcom-caf/common/compatibility_matrix.xml
-DEVICE_MANIFEST_FILE += $(COMMON_PATH)/manifest.xml
-
 # Metadata
 BOARD_USES_METADATA_PARTITION := true
 
