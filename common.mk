449aa907ab6a0809a80e618b66d36ccc1c3f547c
Bruno Martins <bgcngm@gmail.com>
2025-05-31 13:01:12 +0100

    sm7325-common: Move and recategorize compat matrix and manifest flags
    Qualcomm also sets these in (common) product makefiles.
    
    Change-Id: I7139cba92454106fbca5f6105b966a43e70fe9d6

--- common.mk
+++ common.mk
@@ -371,6 +371,13 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     vendor/qcom/opensource/vibrator/excluded-input-devices.xml:$(TARGET_COPY_OUT_VENDOR)/etc/excluded-input-devices.xml
 
+# HIDL
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += \
+    $(LOCAL_PATH)/device_framework_matrix.xml \
+    hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml
+DEVICE_MATRIX_FILE := hardware/qcom-caf/common/compatibility_matrix.xml
+DEVICE_MANIFEST_FILE += $(LOCAL_PATH)/manifest.xml
+
 # WiFi
 PRODUCT_PACKAGES += \
     android.hardware.wifi-service \
