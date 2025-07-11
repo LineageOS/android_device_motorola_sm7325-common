fb0c6470358c60c222c2ae572331e7f00f0c4b04
Nolen Johnson <johnsonnolen@gmail.com>
2025-07-11 18:19:18 -0400

    sm7325-common: Drop QCOM WFD
    * The only thing that QCOM WFD has over AOSP WFD
      is protected buffer (such as Widevine L1) support.
    * Protected buffer casting doesn't work for us,
      likely L1 not working correctly on an unlocked
      device.
    * LineageOS supports AOSP WFD, and these blobs require
      kanging from some new random device every version as
      well as literally like 3 shims. Just not worth the
      technical debt to carry it IMO.
    
    * Additionally for this device family in specific never
      bothered to kang the blobs so it's been broken for
      years!
    
    Change-Id: Ibe80f3afbcde0e28bdff6b47ce711e3e638e35ae

--- extract-files.py
+++ extract-files.py
@@ -50,21 +50,12 @@ lib_fixups: lib_fixups_user_type = {
 }
 
 blob_fixups: blob_fixups_user_type = {
-    'system_ext/bin/wfdservice': blob_fixup()
-        .add_needed('libwfdservice_shim.so'),
     'system_ext/etc/permissions/moto-telephony.xml': blob_fixup()
         .regex_replace('/system/', '/system_ext/'),
     'system_ext/priv-app/ims/ims.apk': blob_fixup()
         .apktool_patch('ims-patches'),
     'vendor/lib64/libwvhidl.so': blob_fixup()
         .add_needed('libcrypto_shim.so'),
-    ('system_ext/lib/libwfdmmsrc_system.so', 'system_ext/lib64/libwfdmmsrc_system.so'): blob_fixup()
-        .add_needed('libgui_shim.so'),
-    'system_ext/lib64/libwfdnative.so': blob_fixup()
-        .add_needed('libbinder_shim.so')
-        .add_needed('libinput_shim.so'),
-    ('system_ext/lib/libwfdservice.so', 'system_ext/lib64/libwfdservice.so'): blob_fixup()
-        .replace_needed('android.media.audio.common.types-V2-cpp.so', 'android.media.audio.common.types-V4-cpp.so'),
     'vendor/lib64/sensors.moto.so': blob_fixup()
         .add_needed('libbase_shim.so'),
 }  # fmt: skip
