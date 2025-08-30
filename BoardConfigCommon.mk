7830539212939920b0dd1f89d396859de034eadf
Yumi Yukimura <me.cafebabe@gmail.com>
2025-08-30 23:45:15 +0800

    sm7325-common: Cleanup more flags
    These are gone after bp conversion for display and media
    
    Change-Id: I1169adf12d717417f5f328ebbcc01ecc9b911889

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -100,9 +100,6 @@ ifeq ($(TARGET_HAS_SNXXX_NFC),)
 DEVICE_MANIFEST_FILE += $(COMMON_PATH)/manifest_nfc.xml
 endif
 
-# Media
-TARGET_USES_ION := true
-
 # Metadata
 BOARD_USES_METADATA_PARTITION := true
 
