158a6c0e7a06ebbe28c3fe82840f6c50acd0e6c2
Marc Bourgoin <themard85@gmail.com>
2025-09-09 22:11:11 -0600

    sm7325-common: Drop NFC 1.2 HIDL manifest entry
    Our devices are switching to AIDL NFC
    
    Change-Id: Iff8e226df0f5aed24bb9adfff8136408481820d8

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -107,9 +107,6 @@ DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
     vendor/lineage/config/device_framework_matrix.xml
 DEVICE_MATRIX_FILE := hardware/qcom-caf/common/compatibility_matrix.xml
 DEVICE_MANIFEST_FILE += $(COMMON_PATH)/manifest.xml
-ifeq ($(TARGET_HAS_SNXXX_NFC),)
-DEVICE_MANIFEST_FILE += $(COMMON_PATH)/manifest_nfc.xml
-endif
 
 # Metadata
 BOARD_USES_METADATA_PARTITION := true
