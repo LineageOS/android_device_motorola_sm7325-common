c456127ea96d26aa5d314133b6283cbdf409aec4
Nolen Johnson <johnsonnolen@gmail.com>
2026-01-22 18:20:42 -0500

    sm7325-common: Convert overlay-lineage to RROs
    Change-Id: I543f078dd4dba30e1b22737d5c22d591e3fb34e6

--- common.mk
+++ common.mk
@@ -22,16 +22,16 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/developer_gsi_keys.mk)
 $(call inherit-product, hardware/qcom-caf/common/common.mk)
 
 # Overlays
-DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay-lineage
-
 PRODUCT_ENFORCE_RRO_TARGETS := *
 
 PRODUCT_PACKAGES += \
+    ApertureOverlayCommon \
     FrameworksResCommon \
     FrameworksResProduct \
     FrameworksResTarget \
+    LineageSDKResCommon \
     NcmTetheringOverlay \
+    SettingsResCommon \
     SystemUIResCommon \
     TelephonyResCommon \
     TelephonyResProduct \
